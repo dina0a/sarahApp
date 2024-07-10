3bd3104ea6d2080e38c5b462325048c2ce2927c8
dina0a <dinamohsen770@gmail.com>
2024-07-10 19:23:46 +0300

    sec commit

--- src/modules/auth/auth.controller.js
+++ src/modules/auth/auth.controller.js
@@ -2,48 +2,40 @@ import bcrypt from 'bcrypt'
 import jwt from 'jsonwebtoken'
 import { User } from '../../../db/models/user.model.js'
 import { sendEmail } from '../../utils/sendEmail.js'
+import { AppError } from '../../utils/appError.js'
 
 // signup
 export const signup = async (req, res, next) => {
-    try {
-        const { userName, email, password } = req.body
-        const userExist = await User.findOne({ email })
-        if (userExist) {
-            throw Error("user alreadu exist", { cause: 401 })
-        }
-        const hashPassword = bcrypt.hashSync(password, 8)
-        const user = new User({
-            userName,
-            email,
-            password: hashPassword
-        })
-        const createdUser = await user.save()
-        createdUser.password = undefined
-        const token = jwt.sign({email},'dinasystem')
-        sendEmail(email,token)
-        return res.status(201).json({ mesaage: "user created successfully", success: true, data: createdUser })
+    const { userName, email, password } = req.body
+    const userExist = await User.findOne({ email })
+    if (userExist) {
+        next(new AppError("user already exist", 409))
     }
-    catch (err) {
-        return res.status(err.cause || 500).json({ mesaage: err.message, success: false })
-    }
+    const hashPassword = bcrypt.hashSync(password, 8)
+    const user = new User({
+        userName,
+        email,
+        password: hashPassword
+    })
+    const createdUser = await user.save()
+    createdUser.password = undefined
+    const token = jwt.sign({ email }, 'dinasystem')
+    sendEmail(email, token)
+    return res.status(201).json({ mesaage: "user created successfully", success: true, data: createdUser })
 }
+
 
 // signIn
 export const signIn = async (req, res, next) => {
-    try {
-        const { email, password } = req.body
-        const userExist = await User.findOne({ email })
-        if (!userExist) {
-            throw Error("invalid credentials", { cause: 401 })
-        }
-        const match = bcrypt.compareSync(password, userExist.password)
-        if (!match) {
-            throw Error("invalid credentials", { cause: 401 })
-        }
-        const accessToken = jwt.sign({ userId: userExist._id, userName: userExist.userName }, 'dinasystem')
-        return res.status(200).json({ mesaage: "logIn successfully", success: true, token: accessToken })
+    const { email, password } = req.body
+    const userExist = await User.findOne({ email })
+    if (!userExist) {
+        next(new AppError("invalid credentials", 401))
     }
-    catch (err) {
-        return res.status(err.cause || 500).json({ mesaage: err.message, success: false })
+    const match = bcrypt.compareSync(password, userExist.password)
+    if (!match) {
+        next(new AppError("invalid credentials", 401))
     }
+    const accessToken = jwt.sign({ userId: userExist._id, userName: userExist.userName }, 'dinasystem')
+    return res.status(200).json({ mesaage: "logIn successfully", success: true, token: accessToken })
 }
--- src/modules/message/message.router.js
+++ src/modules/message/message.router.js
@@ -1,11 +1,12 @@
 import { Router } from "express";
 import { addMessage, deleteMessage, getMessage } from "./message.controller.js";
 import { auth } from "../../middleware/authentication.js";
+import { asyncHandler } from "../../utils/asyncHandler.js";
 
 const messageRouter = Router()
 
-messageRouter.post('/',auth,addMessage)
-messageRouter.get('/',auth,getMessage)
-messageRouter.delete('/:id',auth,deleteMessage)
+messageRouter.post('/', auth, asyncHandler(addMessage))
+messageRouter.get('/', auth, asyncHandler(getMessage))
+messageRouter.delete('/:id', auth, asyncHandler(deleteMessage))
 
 export { messageRouter }
